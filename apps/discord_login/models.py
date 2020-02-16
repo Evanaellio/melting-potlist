7f5086ce33980aa76a88fa72a3884dee9dd4a23b
Evanaellio <evan.mottais@gmail.com>
2020-02-16 16:54:23 +0100

    Add image property to DiscordUser and DiscordGuild

--- apps/discord_login/models.py
+++ apps/discord_login/models.py
@@ -7,6 +7,15 @@ class DiscordGuild(models.Model):
     name = models.TextField()
     icon = models.TextField(blank=True)
 
+    @property
+    def image(self):
+        if self.icon:
+            icon_ext = 'gif' if self.icon.startswith('a_') else 'png'
+            image = f"https://cdn.discordapp.com/icons/{self.id}/{self.icon}.{icon_ext}"
+        else:
+            image = 'https://cdn.discordapp.com/embed/avatars/0.png'
+        return image
+
 
 class DiscordUser(models.Model):
     id = models.BigAutoField(primary_key=True)
@@ -16,3 +25,12 @@ class DiscordUser(models.Model):
     username = models.TextField()
     discriminator = models.TextField()
     locale = models.TextField()
+
+    @property
+    def image(self):
+        if self.avatar:
+            icon_ext = 'gif' if self.avatar.startswith('a_') else 'png'
+            image = f'https://cdn.discordapp.com/avatars/{self.id}/{self.avatar}.{icon_ext}'
+        else:
+            image = f'https://cdn.discordapp.com/embed/avatars/{self.discriminator % 5}.png'
+        return image
