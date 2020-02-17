99c1e304d119e4e10b695a8dbbe1ac6aa139e458
Evanaellio <evan.mottais@gmail.com>
2020-02-17 19:54:23 +0100

    Add gunicorn and whitenoise for production server

--- melting_potlist/settings.py
+++ melting_potlist/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -121,6 +122,8 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
+
+STATIC_ROOT = BASE_DIR / "staticfiles"
 
 AUTHENTICATION_BACKENDS = [
     'apps.discord_login.backends.DiscordBackend',
