2659a70cdf1949ee7a27c3dddb849d1cf68fc199
Evanaellio <evan.mottais@gmail.com>
2020-02-19 13:09:54 +0000

    Disable users who don't yet have their playlist configured

--- apps/core/views.py
+++ apps/core/views.py
@@ -31,6 +31,7 @@ def make_user(user: DiscordUser):
         'id': str(user.id),
         'name': user.username,
         'image': user.image,
+        '$isDisabled': not user.user.settings.core_playlist_url,
     }
 
 
@@ -55,7 +56,10 @@ def groups(request):
 @login_required
 def group_playlist(request, guild_id):
     guild = get_object_or_404(DiscordGuild, id=guild_id)
-    users = list(map(make_user, guild.users.all()))
+    
+    valid_users = filter(guild.users.all())
+    
+    valid_users = list(map(make_user, guild.users.all()))
 
     context = {
         'users_json': json.dumps(users),
