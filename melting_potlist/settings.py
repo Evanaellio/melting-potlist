9803a4c3634d2753ab6cdceb66242afad5c40e0c
Evanaellio <evan.mottais@gmail.com>
2020-10-01 02:00:15 +0200

    Add new interfaces for multiple playlist view and playlist detail

--- melting_potlist/settings.py
+++ melting_potlist/settings.py
@@ -35,6 +35,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.humanize',
     'django_yamlconf',
     'widget_tweaks',
     'apps.core',
