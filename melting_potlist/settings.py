13b7aa0d8c2c97461817aa54ed90a23fb5031dac
Evanaellio <evan.mottais@gmail.com>
2021-10-20 19:41:55 +0200

    Init Django Rest Framework

--- melting_potlist/settings.py
+++ melting_potlist/settings.py
@@ -38,10 +38,12 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'django.contrib.humanize',
     'django_yamlconf',
+    'rest_framework',
     'widget_tweaks',
     'apps.core',
     'apps.discord_login',
     'apps.user_profile',
+    'apps.api',
 ]
 
 MIDDLEWARE = [
@@ -104,6 +106,16 @@ AUTH_PASSWORD_VALIDATORS = [
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
+
+# Django Rest Framework
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.SessionAuthentication',
+    ],
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated',
+    ],
+}
 
 # Internationalization
 # https://docs.djangoproject.com/en/3.0/topics/i18n/
