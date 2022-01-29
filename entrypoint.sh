118639486fd775acd0cd0d15bdbe6a0f66206cce
Evanaellio <evan.mottais@gmail.com>
2022-01-29 18:57:13 +0100

    Add uvicorn, daphne sucks

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,5 +7,6 @@ python manage.py migrate
 python manage.py collectstatic
 
 # Start Django server
+gunicorn melting_potlist.asgi:application -w 4 -k uvicorn.workers.UvicornWorker
 # gunicorn melting_potlist.wsgi:application --bind 0.0.0.0:8000
-daphne -b 0.0.0.0 -p 8000 melting_potlist.asgi:application
+# daphne -b 0.0.0.0 -p 8000 melting_potlist.asgi:application
