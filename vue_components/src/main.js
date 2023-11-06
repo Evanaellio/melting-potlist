7ba4e97f894bb508bde5b4dee08c772863b16239
Evanaellio <evan.mottais@gmail.com>
2023-11-06 23:37:05 +0100

    Try without dyn imports

--- vue_components/src/main.js
+++ vue_components/src/main.js
@@ -1,4 +1,9 @@
-import {createApp, defineAsyncComponent} from "vue";
+import {createApp} from "vue";
+import PlaylistHost from "./pages/play_dynamic_playlist_host.vue";
+import PlaylistListener from "./pages/play_dynamic_playlist_listener.vue";
+import CreatePlaylist from "./pages/create_dynamic_playlist.vue";
+import Groups from "./pages/groups.vue";
+
 
 const app = createApp({});
 
@@ -6,8 +11,8 @@ app.config.globalProperties.$window = window;
 window.vue_app = app; // Used to debug vue app in production
 
 // Register pages as components
-app.component("play-dynamic-playlist-host", defineAsyncComponent(() => import("./pages/play_dynamic_playlist_host.vue")))
-    .component("play-dynamic-playlist-listener", defineAsyncComponent(() => import("./pages/play_dynamic_playlist_listener.vue")))
-    .component("create-dynamic-playlist", defineAsyncComponent(() => import("./pages/create_dynamic_playlist.vue")))
-    .component("groups", defineAsyncComponent(() => import("./pages/groups.vue")))
+app.component("play-dynamic-playlist-host", PlaylistHost)
+    .component("play-dynamic-playlist-listener", PlaylistListener)
+    .component("create-dynamic-playlist", CreatePlaylist)
+    .component("groups", Groups)
     .mount("#vue_app");
