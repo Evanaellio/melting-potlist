70040458b878ba48615736bc80ef3d68f0cee544
Evanaellio <evan.mottais@gmail.com>
2023-11-06 21:08:13 +0100

    Other fix test

--- vue_components/src/main.js
+++ vue_components/src/main.js
@@ -2,14 +2,12 @@ import {createApp, defineAsyncComponent} from "vue";
 
 const app = createApp({});
 
-// Register pages as components
-app.component("play-dynamic-playlist-host", defineAsyncComponent(() => import("./pages/play_dynamic_playlist_host.vue")));
-app.component("play-dynamic-playlist-listener", defineAsyncComponent(() => import("./pages/play_dynamic_playlist_listener.vue")));
-app.component("create-dynamic-playlist", defineAsyncComponent(() => import("./pages/create_dynamic_playlist.vue")));
-app.component("groups", defineAsyncComponent(() => import("./pages/groups.vue")));
+app.config.globalProperties.$window = window;
+window.vue_app = app; // Used to debug vue app in production
 
-export default function mountApp() {
-    app.config.globalProperties.$window = window;
-    window.vue_app = app; // Used to debug vue app in production
-    app.mount("#vue_app");
-}
+// Register pages as components
+app.component("play-dynamic-playlist-host", defineAsyncComponent(() => import("./pages/play_dynamic_playlist_host.vue")))
+    .component("play-dynamic-playlist-listener", defineAsyncComponent(() => import("./pages/play_dynamic_playlist_listener.vue")))
+    .component("create-dynamic-playlist", defineAsyncComponent(() => import("./pages/create_dynamic_playlist.vue")))
+    .component("groups", defineAsyncComponent(() => import("./pages/groups.vue")))
+    .mount("#vue_app");
